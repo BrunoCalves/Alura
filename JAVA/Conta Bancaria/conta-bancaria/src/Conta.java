ca6ca9a14804630b1fa64c9ace43085c6f42fd85
Bruno Alves <bruno_.alves@outlook.com>
2023-07-19 13:45:05 -0300

    Criando a classe Conta e os metodos get e set para o saldo

---
+++ JAVA/Conta Bancaria/conta-bancaria/src/Conta.java
@@ -0,0 +1,10 @@
+public class Conta {
+        private double saldo = 0.2; //valor de teste
+
+        public void setSaldo(Double saldo){
+            this.saldo = saldo;
+        }
+        public Double getSaldo(){
+            return saldo;
+        }
+}
