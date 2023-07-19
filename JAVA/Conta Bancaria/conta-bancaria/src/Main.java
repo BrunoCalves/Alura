66aca3850fb0e123697ca7fdb47bbf938ababafb
Bruno Alves <bruno_.alves@outlook.com>
2023-07-19 00:13:23 -0300

    criando o menu do programa

--- JAVA/Conta Bancaria/conta-bancaria/src/Main.java
+++ JAVA/Conta Bancaria/conta-bancaria/src/Main.java
@@ -1,6 +1,23 @@
 
 public class Main {
+    static void menu(){
+        String textoMenu = """
+               ****************************************
+                          Bem vindo ao Banco!
+               
+               1 - Verificar Saldo
+               2 - Depositar
+               3 - Sacar
+               4 - Transferir
+               5 - Sair
+               
+               ****************************************
+                """;
+
+        System.out.println(textoMenu);
+    }
     public static void main(String[] args) {
         System.out.println("Inciando o projeto conta bancaria!");
+        menu();
     }
 }
