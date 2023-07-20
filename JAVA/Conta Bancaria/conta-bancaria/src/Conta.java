1dec5c7e79cd0a77dd9580dd8dcfca87f293d920
Bruno Alves <bruno_.alves@outlook.com>
2023-07-19 21:58:11 -0300

    Finalizando o projeto conta bancaria com a criação dos metodos sacar, transferir e ferificar saldo

--- JAVA/Conta Bancaria/conta-bancaria/src/Conta.java
+++ JAVA/Conta Bancaria/conta-bancaria/src/Conta.java
@@ -1,10 +1,31 @@
 public class Conta {
         private double saldo = 0.2; //valor de teste
 
-        public void setSaldo(Double saldo){
+        public void setSaldo(Double saldo) {
             this.saldo = saldo;
         }
         public Double getSaldo(){
             return saldo;
+        }
+
+        public void consultaSaldo(){
+            System.out.println("\n\nSaldo disponível: "+ getSaldo());
+        }
+        public void depositar(Double valor){
+            double atualizaSaldo;
+            atualizaSaldo = getSaldo() + valor;
+            setSaldo(atualizaSaldo);
+            System.out.println("Operação realizada com sucesso! ");
+        }
+
+        public void transferir(Double valor){
+            double atualizaSaldo;
+            if(getSaldo() >= valor){
+                atualizaSaldo = getSaldo() - valor;
+                setSaldo(atualizaSaldo);
+                System.out.println("Operação realizada com sucesso! ");
+            }else {
+                System.out.println("Saldo insuficiente! ");
+            }
         }
 }
--- JAVA/Conta Bancaria/conta-bancaria/src/Main.java
+++ JAVA/Conta Bancaria/conta-bancaria/src/Main.java
@@ -1,29 +1,64 @@
+import java.util.Scanner;
 
 public class Main {
-    static void menu(){
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        Scanner scanner = new Scanner(System.in);
+        double deposito;
+        int opcao;
         String textoMenu = """
-               ****************************************
-                          Bem vindo ao Banco!
+               
                
                1 - Verificar Saldo
                2 - Depositar
                3 - Sacar
-               4 - Transferir
-               5 - Sair
+               4 - Sair
+               
+               
+                """;
+
+        String dadosConta = """
+               ****************************************
+                          Bem-vindo ao Banco!
+               
+               Nome: Bruno César Alves
+               Tipo de conta: Poupança
+               Saldo disponível: %s
                
                ****************************************
-                """;
-        System.out.println(textoMenu);
-    }
-    public static void main(String[] args) {
-        System.out.println("Inciando o projeto conta bancaria!");
-        Conta conta = new Conta();
-
-        System.out.println(conta.getSaldo());
-        conta.setSaldo(2.50);
-        System.out.println(conta.getSaldo());
+                """.formatted(conta.getSaldo());
 
 
+        System.out.println(dadosConta);
 
+
+        do{
+            System.out.println(textoMenu);
+            System.out.println("\nDigite a opção desejada: ");
+            opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    conta.consultaSaldo();
+                    break;
+                case 2:
+                    System.out.println("Digite o valor do deposito: ");
+                    deposito = scanner.nextDouble();
+                    conta.depositar(deposito);
+                    conta.consultaSaldo();
+                    break;
+                case 3:
+                    System.out.println("Digite o valor da transferencia: ");
+                    deposito = scanner.nextDouble();
+                    conta.transferir(deposito);
+                    conta.consultaSaldo();
+                    break;
+                case 4:
+                    System.out.println("Obrigado por utilizar BRN bank! ");
+                    System.out.println("****************************************");
+                    break;
+                default:
+                    System.out.println("Opção invalida!");
+            }
+        }while (opcao != 4);
     }
 }
